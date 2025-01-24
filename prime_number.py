a342d5e217006d6e3fae25f448f13a11bc0a1a7f
vikasraog6 <vikasraog6@gmail.com>
2025-01-23 23:06:53 -0600

    changes done in the prime number

--- prime_number.py
+++ prime_number.py
@@ -6,7 +6,7 @@ for i in range (2, number):
     if(number % i == 0):
         flag = True
         break
-    if flag:
-            print("Entered number {} is not a prime number".format(number))
-    else:
-        print("Entered number {} is a prime number".format(number))
+if flag:
+    print("Entered number {} is not a prime number".format(number))
+else:
+    print("Entered number {} is a prime number".format(number))
