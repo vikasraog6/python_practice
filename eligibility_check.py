475c4f7b95185abc26c0758ce392fb5ed46a6d7d
vikasraog6 <vikasraog6@gmail.com>
2025-01-28 16:41:44 -0600

    new

---
+++ eligibility_check.py
@@ -0,0 +1,14 @@
+sex = input("Enter sex (male, female, m, f ) :")
+age = int(input("Enter age : "))
+if sex.lower() in ("female", "f"):
+    if age < 18:
+        print("Not Eligible For Marriage")
+    else:
+        print("Eligible For Marriage")
+elif sex.lower() in ("male", "m"):
+    if age >= 18:
+        print("Eligible For Marriage")
+    else:
+        print("Not Eligible For Marriage")
+else:
+    print("We dont know")
